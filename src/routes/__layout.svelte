009fcb1f24a7814d0929fec6364c067188753699
Matthew Honour <matthewpeterhonour@gmail.com>
2022-04-03 18:44:54 +0100

    Refactored styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,40 +1,22 @@
 <script>
-    import '../app.css';
+  import '../app.css';
 </script>
 
 <main>
-    <slot />
+  <slot />
 </main>
 
-<footer>
-    <p>v0.0.1</p>
-</footer>
-
 <style>
-    main {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        padding: 1rem;
-        width: 100%;
-        max-width: 1024px;
-        margin: 0 auto;
-        box-sizing: border-box;
-        font-family: 'Courier New', Courier, monospace;
-    }
-
-    footer {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 40px;
-        font-family: 'Courier New', Courier, monospace;
-    }
-
-    @media (min-width: 480px) {
-        footer {
-            padding: 40px 0;
-        }
-    }
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    height: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+    font-family: 'Courier New', Courier, monospace;
+  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,38 @@
 <script>
-    import PlayerTile from '../lib/playerTile.svelte';
+  import PlayerTile from '../lib/playerTile.svelte';
+
+  let players = [{ name: 'Player 1' }, { name: 'Player 2' }];
 </script>
 
 <div class="home-wrapper">
-    <h1>Cheese on Wheels</h1>
-    <div>
-        <PlayerTile playerName="Player 1" />
-        <PlayerTile playerName="Player 2" />
-    </div>
+  <h1>Cheese on Wheels</h1>
+  <div class="playerTiles">
+    {#each players as player}
+      <PlayerTile playerName={player.name} />
+    {/each}
+  </div>
 </div>
 
 <style>
-    h1 {
-        text-align: center;
-        font-weight: 600;
-        font-size: 2.5em;
-    }
+  h1 {
+    text-align: center;
+    font-weight: 600;
+    font-size: 2.5em;
+  }
 
-    .home-wrapper {
-        display: flex;
-        justify-content: start;
-        gap: 100px;
-        flex-direction: column;
-        height: 100%;
-    }
+  .home-wrapper {
+    display: flex;
+    justify-content: start;
+    /* gap: 10%; */
+    flex-direction: column;
+    height: 100%;
+  }
+
+  .playerTiles {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    gap: 5%;
+    flex-direction: column;
+  }
 </style>
